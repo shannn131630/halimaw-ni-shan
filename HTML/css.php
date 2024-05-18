e1a594709d28483e502cfd53e5fc9baa0a3d6184
shannn131630 <sllancero2006@gmail.com>
2024-05-18 15:10:24 +0800

    Add files via upload

---
+++ HTML/css.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Based ICT Dictionary</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header>
+      <h2 class="logo">ICT DICTIONARY</h2>  
+      <form>
+            <input type="search" id="searchbox" class="searchbox" placeholder="Search our site">
+          </form>
+      <nav class="navigation">
+        <a href="html.php">HTML</a>
+        <a href="css.php">CSS</a>
+        <a href="JS.php">JavaScript</a>
+        <a href="PHP">PHP</a>
+        <button class="btnLogin-popup">Login</button>
+        </nav>
+        <div class="O"></div>
+    <div class="Sidebar">
+        <h3>Assignment</h3>
+        <ul>
+            <li><a href="Home.html"><i class='bx bx-home' ></i>Home</a></li>
+            <li><a href="Dashboard.html"><i class='bx bxs-dashboard'></i>Dashboard</a></li>
+            <li><a href="Assignment.html"><i class='bx bx-book'></i>Assignment</a></li>
+            <li><a href="Files.html"><i class='bx bx-import' ></i>Files</a></li>
+            <li><a href="Setting.html"><i class='bx bx-cog'></i>Settings</a></li>
+            <li><a href="index.html"><i class='bx bxs-log-out'></i>LogOut</a></li>
+        </ul>
+        </div>
+</div>
+    </header>
+    
+    <div class="wrapper">
+        <span class="icon-close">
+            <ion-icon name="close"></ion-icon>
+        </span>
+
+        <div class="form-box login">
+            <h2>Login</h2>
+            <form action="#"> 
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="mail"></ion-icon>
+                    </span>
+                    <input type="email" required>
+                    <label>Email</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="lock-closed">
+                        </ion-icon>
+                    </span>
+                    <input type="password" required>
+                    <label>Password</label>
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox">Remember me</label>
+                    <a href="#" >Forgot Password?</a>               
+                 </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="login-register">
+                    <p>Don't have an account? <a href="#" class="register-link">Register</a></p>
+                </div>
+            </form>
+        </div>
+
+        <div class="form-box register">
+            <h2>Registration</h2>
+            <form action="#"> 
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="person"></ion-icon>
+                    </span>
+                    <input type="text" required>
+                    <label>Username</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="mail"></ion-icon>
+                    </span>
+                    <input type="email" required>
+                    <label>Email</label>
+                </div>
+                
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="lock-closed">
+                        </ion-icon>
+                    </span>
+                    <input type="password" required>
+                    <label>Password</label>
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox">I agree to the terms & conditions</label>
+                </div>
+                <button type="submit" class="btn">Register</button>
+                <div class="login-register">
+                    <p>Already have an account? <a href="#" class="login-link">Login</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="script.js"></script>   
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+   
+</body>
+</html>
